e53c0a686c5931b14025bc8a0da8787e96b1c5f8
Getasew Tewachew <getasewtewachew@gmail.com>
2025-10-22 13:35:13 -0700

    final change

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,11 @@
 import mongoose, { mongo } from "mongoose";
+import env from "dotenv";
+env.config()
  
 export const conneDb=async () => {
     try {
         const conn=await mongoose.connect("mongodb+srv://geach9007_db_user:KYdmcpeQnjv17tg7@cluster0.mwhbxlf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-        console.log("mongo db connected host is: "+conn.connection)
+        console.log("mongo db connected host is: "+conn.connection.host)
     } catch (error) {
         console.log('Faild connect to mongodb',error)
         process.exit(1)
